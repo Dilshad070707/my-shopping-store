fb75da9d769e094f3808e306134e9027d74d4a67
Dilshad070707 <dilshadattari07@gmail.com>
2026-08-11 16:05:32 +0530

    Remove strictPort from server and preview settings
    Removed strictPort option from server and preview configurations.

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,9 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
-    strictPort: true
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: 4173,
-    strictPort: true
   },
   build: {
     outDir: "dist",
     sourcemap: false,
-    emptyOutDir: true
-  }
+  },
 });
